6f5957f408087234aa0838a72f707d516ff69645
tarun142605 <tarun142605@gmail.com>
2024-07-18 12:55:32 +0530

    first changes

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Left_panel from './components/Left_panel'
+import './assets/Main.css'
+// import App from './App.jsx'
+// import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Left_panel />
+
+  </React.StrictMode>,
+)
